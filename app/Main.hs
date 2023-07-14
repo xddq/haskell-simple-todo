63ef27876f93c410b6a1ce551a653e4bd5b31119
Pierre Dahmani <hi@pierre-dev.com>
2023-07-14 23:34:19 +0200

    implement create update delete using postgresql-simple

--- app/Main.hs
+++ app/Main.hs
@@ -3,31 +3,21 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
--- An example of embedding a custom monad into
--- Scotty's transformer stack, using ReaderT to provide access
--- to a TVar containing global state.
---
--- Note: this example is somewhat simple, as our top level
--- is IO itself. The types of 'scottyT' and 'scottyAppT' are
--- general enough to allow a Scotty application to be
--- embedded into any MonadIO monad.
 module Main (main) where
 
-import Control.Concurrent.STM
 import Control.Monad.Reader
 import Data.Aeson (FromJSON (parseJSON), ToJSON (toJSON), decode, encode, object, withObject, (.:), (.:?), (.=))
-import Data.Default.Class
 import Data.Int
-import qualified Data.Map.Strict as M
+import Data.Maybe (listToMaybe)
 import Data.String
-import Data.Text.Lazy (Text, pack, strip, unpack)
+import Data.Text.Lazy (Text, pack)
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import Data.Text.Lazy.Read
 import Database.PostgreSQL.Simple
 import Database.PostgreSQL.Simple.FromRow
 import Database.PostgreSQL.Simple.ToRow
 import GHC.Generics (Generic)
-import Network.HTTP.Types (Status, status200, status400, status404)
+import Network.HTTP.Types (Status, status200, status400, status404, status500)
 import Network.Wai.Middleware.Cors
 import Network.Wai.Middleware.RequestLogger
 import Prelude.Compat
@@ -36,15 +26,11 @@ import Prelude ()
 
 -- Client facing errors. Whenever we get an error, we return additional
 -- information in this format.
-data ApiError = ApiError
-  { apiErrrorMessage :: Text
-  }
+newtype ApiError = ApiError {apiErrrorMessage :: Text}
   deriving (Show, Generic)
 
 instance ToJSON ApiError where
-  toJSON (ApiError msg) =
-    object
-      ["message" .= msg]
+  toJSON (ApiError msg) = object ["message" .= msg]
 
 -- for validating the todo we pass when creating a new todo
 data CreateTodoInput = CreateTodoInput
@@ -64,23 +50,37 @@ instance FromJSON CreateTodoInput where
     createTodoInputFromJsonDone <- obj .: "done"
     return (CreateTodoInput createTodoInputFromJsonText createTodoInputFromJsonDone)
 
--- -- TODO: adapt so here we can have have either updateTodoText or updateTodoDone or both
--- data UpdateTodoInput = UpdateTodoInput
---   {updateTodoText :: Maybe String, updateTodoDone :: Maybe Bool}
---   deriving (Show, Generic)
---
--- instance FromJSON UpdateTodoInput where
---   parseJSON = withObject "UpdateTodoInput" $ \obj -> do
---     newText <- obj .:? "text"
---     newDone <- obj .:? "done"
---     return (UpdateTodoInput {updateTodoText = newText, updateTodoDone = newDone})
+-- for validating the todo we pass when updating a todo
+data UpdateTodoInput = UpdateTodoInput
+  { updateTodoInputId :: Int,
+    updateTodoInputText :: String,
+    updateTodoInputDone :: Bool
+  }
+  deriving (Show, Generic)
+
+instance ToRow UpdateTodoInput where
+  -- NOTE: the order here (in the toRow on the right) determines the required
+  -- order of args in the SQL insert statement.
+  toRow UpdateTodoInput {updateTodoInputText, updateTodoInputDone, updateTodoInputId} = toRow (updateTodoInputText, updateTodoInputDone, updateTodoInputId)
+
+instance FromJSON UpdateTodoInput where
+  parseJSON = withObject "CreateTodoInput" $ \obj -> do
+    updateTodoInputFromJsonId <- obj .: "id"
+    updateTodoInputFromJsonText <- obj .: "text"
+    updateTodoInputFromJsonDone <- obj .: "done"
+    return (UpdateTodoInput updateTodoInputFromJsonText updateTodoInputFromJsonDone updateTodoInputFromJsonId)
 
 sendError :: Text -> Status -> ActionM ()
 sendError message responseStatus = do
-  -- TODO: perhaps set header globally?
   setHeader "Content-Type" "application/json"
   status responseStatus
   text $ decodeUtf8 $ encode $ ApiError message
+
+sendSuccess :: Text -> ActionM ()
+sendSuccess message = do
+  setHeader "Content-Type" "application/json"
+  status status200
+  text message
 
 data Todo = Todo
   { todoId :: Int,
@@ -92,15 +92,33 @@ data Todo = Todo
 instance FromRow Todo where
   fromRow = Todo <$> field <*> field <*> field
 
+instance FromJSON Todo
+
+instance ToJSON Todo where
+  toJSON (Todo todoToJsonId todoToJsontext todoToJsonDone) =
+    object
+      [ "id" .= todoToJsonId,
+        "text" .= todoToJsontext,
+        "done" .= todoToJsonDone
+      ]
+
+createTodo :: Connection -> CreateTodoInput -> IO [Todo]
+createTodo conn = query conn "INSERT INTO todos (done,text) VALUES (?,?) RETURNING *"
+
+getTodos :: Connection -> IO [Todo]
+getTodos conn = query_ conn "SELECT * FROM todos"
+
 getTodoById :: Connection -> Int -> IO [Todo]
 getTodoById conn idOfTodo = query conn "SELECT * FROM todos WHERE id = ?" [idOfTodo]
 
--- TODO: why does this not work??
+-- TODO: why does this one not work??
 -- getTodoById conn idOfTodo = query conn "SELECT (id,text,done) FROM todos WHERE id = ?" [idOfTodo]
 
--- TODO: add RETURNING and return the created todo.
-createTodo :: Connection -> CreateTodoInput -> IO Int64
-createTodo conn = execute conn "INSERT INTO todos (done,text) VALUES (?,?)"
+updateTodoById :: Connection -> UpdateTodoInput -> IO [Todo]
+updateTodoById conn = query conn "UPDATE todos SET text=?,done=? WHERE id=? RETURNING *"
+
+deleteTodo :: Connection -> Int -> IO Int64
+deleteTodo conn todoId = execute conn "DELETE FROM todos WHERE id = ?" (Only todoId)
 
 main :: IO ()
 main = do
@@ -114,139 +132,61 @@ main = do
     get "/" $ do
       text $ fromString "Welcome to your todo list! You might want to query /todos instead :]"
 
-    get "/4" $ do
-      [Only result] <- liftIO (query_ conn "select 2 + 2" :: IO [Only Int])
-      text $ pack $ show result
+    -- GET all todos
+    get "/todos" $ do
+      todos <- liftIO (getTodos conn)
+      sendSuccess (decodeUtf8 $ encode todos)
 
+    -- GET one todo
     get "/todos/:id" $ do
       unparsedId <- param "id"
       case decimal unparsedId of
         Left err -> sendError (pack err) status400
         Right (parsedId, _rest) -> do
-          -- TODO: why do we get error "Incompatible {errSQLType = "record",
-          -- errSQLTableOid = Nothing, errSQLField = "row", errHaskellType =
-          -- "Int", errMessage = "types incompatible"}" here?
-          todo <- liftIO (getTodoById conn parsedId)
-          setHeader "Content-Type" "application/json"
-          status status200
-          text $ decodeUtf8 $ encode todo
+          listWithTodo <- liftIO (getTodoById conn parsedId)
+          case listToMaybe listWithTodo of
+            Just todo -> sendSuccess $ decodeUtf8 $ encode todo
+            Nothing -> sendError "not found" status404
 
-    -- CREATE todo
+    -- DELETE one todo
+    delete "/todos/:id" $ do
+      unparsedId <- param "id"
+      case decimal unparsedId of
+        Left err -> sendError (pack err) status400
+        Right (parsedId, _rest) -> do
+          deletedRowsCount <- liftIO (deleteTodo conn parsedId)
+          if deletedRowsCount == 1 then sendSuccess "ok" else sendError "not found" status404
+
+    -- CREATE one todo
     post "/todos" $ do
       unparsedJson <- body
       case decode unparsedJson :: Maybe CreateTodoInput of
         Just createTodoInput -> do
-          affectedRows <- liftIO (createTodo conn createTodoInput)
-          setHeader "Content-Type" "application/json"
-          text $ pack $ show affectedRows
-        Nothing -> do
-          status status400
-          text "invalid input"
+          listWithCreatedTodo <- liftIO (createTodo conn createTodoInput)
+          case listToMaybe listWithCreatedTodo of
+            Just createdTodo -> sendSuccess $ decodeUtf8 $ encode createdTodo
+            Nothing -> sendError "should never happen. creating a todo returned an empty list" status500
+        Nothing -> sendError "invalid input" status400
 
-instance FromJSON Todo
+    -- UPDATE one todo
+    patch "/todos/:id" $ do
+      unparsedIdFromPath <- param "id"
+      case decimal unparsedIdFromPath of
+        Left err -> sendError (pack err) status400
+        Right (idOfToBeUpdatedTodo, _rest) -> do
+          unparsedJson <- body
+          case decode unparsedJson :: Maybe UpdateTodoInput of
+            Just updateTodoInput -> do
+              if idOfToBeUpdatedTodo /= updateTodoInputId updateTodoInput
+                then sendError "id of payload and path did not match" status400
+                else do
+                  updatedTodo <- liftIO (updateTodoById conn updateTodoInput)
+                  case listToMaybe updatedTodo of
+                    Just todo -> sendSuccess $ decodeUtf8 $ encode todo
+                    Nothing -> sendError "should never happen. updating a todo returned an empty list" status500
+            Nothing -> sendError "invalid input" status400
 
-instance ToJSON Todo where
-  toJSON (Todo todoToJsonId todoToJsontext todoToJsonDone) =
-    object
-      [ "id" .= todoToJsonId,
-        "text" .= todoToJsontext,
-        "done" .= todoToJsonDone
-      ]
-
--- -- GET all todos
--- get "/todos" $ do
---   c <- webM $ gets todo
---   setHeader "Content-Type" "application/json"
---   status status200
---   text $ strip $ fromString $ unpack $ decodeUtf8 $ encode $ M.foldr (:) [] c
---
--- -- GET todo
--- get "/todos/:id" $ do
---   unparsedId <- param "id"
---   case decimal unparsedId of
---     Left err -> sendError (pack err) status400
---     Right (parsedId, _rest) -> do
---       todos <- webM $ gets todo
---       case M.lookup parsedId todos of
---         Nothing -> sendError "no todo found for given id" status404
---         Just existingTodo -> do
---           setHeader "Content-Type" "application/json"
---           status status200
---           text $ strip $ fromString $ unpack $ decodeUtf8 $ encode existingTodo
---
--- -- DELETE todo
--- delete "/todos/:id" $ do
---   unparsedId <- param "id"
---   case decimal unparsedId of
---     Left err -> sendError (pack err) status400
---     Right (parsedId, _rest) -> do
---       todos <- webM $ gets todo
---       case M.lookup parsedId todos of
---         Just toDeleteTodo -> do
---           webM $ modify $ \st -> st {todo = M.delete parsedId $ todo st}
---           status status200
---           setHeader "Content-Type" "application/json"
---           text $ strip $ fromString $ unpack $ decodeUtf8 $ encode toDeleteTodo
---         Nothing -> sendError "no todo found for given id" status404
---
--- -- CREATE todo
--- post "/todos" $ do
---   unparsedJson <- body
---   case decode unparsedJson :: Maybe CreateTodoInput of
---     Just createTodoInput -> do
---       todos <- webM $ gets todo
---       -- MAYBE: make them functions and use as "with"
---       let idOfNewTodo = 1 + fst (M.findMax todos)
---       let createdTodo = Todo (__text createTodoInput) idOfNewTodo False
---       webM $ modify $ \st -> st {todo = M.insert idOfNewTodo createdTodo $ todo st}
---       setHeader "Content-Type" "application/json"
---       text $ todoToJsonText createdTodo
---     Nothing -> do
---       status status400
---       text "invalid input"
---
--- -- UPDATE todo
--- patch "/todos/:id" $ do
---   unparsedIdFromPath <- param "id"
---   case decimal unparsedIdFromPath of
---     Left err -> sendError (pack err) status400
---     Right (idOfTodoToBeUpdated, _rest) -> do
---       unparsedJson <- body
---       case decode unparsedJson :: Maybe UpdateTodoInput of
---         Just updateTodoInput -> do
---           todos <- webM $ gets todo
---           case M.lookup idOfTodoToBeUpdated todos of
---             Just currentTodo -> do
---               let updatedTodo = updateTodo currentTodo updateTodoInput
---               webM $ modify $ \st -> st {todo = M.insert idOfTodoToBeUpdated updatedTodo $ todo st}
---               setHeader "Content-Type" "application/json"
---               text $ todoToJsonText updatedTodo
---             Nothing -> sendError "no todo found for given id" status404
---         Nothing -> sendError "invalid input" status400
-
--- TODO: How is this done "correctly"? With this approach, we would need a lot
--- of different cases if we had e.g. 5 optional attributes..?
--- updateTodo :: Todo -> UpdateTodoInput -> Todo
--- updateTodo currentTodo newTodo = do
---   let newTodoText = updateTodoText newTodo
---   let newTodoDone = updateTodoDone newTodo
---   case (newTodoText, newTodoDone) of
---     (Just newText, Just newDone) -> Todo newText (_id currentTodo) newDone
---     (Just newText, Nothing) -> Todo newText (_id currentTodo) (_done currentTodo)
---     (Nothing, Just newDone) -> Todo (_text currentTodo) (_id currentTodo) newDone
---     (Nothing, Nothing) -> currentTodo
-
--- todoToJsonText :: Todo -> Text
--- todoToJsonText = fromString . unpack . decodeUtf8 . encode
-
--- sendError :: Text -> Status -> ActionT Text WebM ()
--- sendError message responseStatus = do
---   -- TODO: perhaps set header globally?
---   setHeader "Content-Type" "application/json"
---   status responseStatus
---   text $ decodeUtf8 $ encode $ ApiError message
-
--- TODO: why is this not working?
+-- TODO: why is this signature not working?
 -- allowCors :: Middleware
 allowCors = cors (const $ Just appCorsResourcePolicy)
 
